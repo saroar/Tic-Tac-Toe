5093633166b0c682796accfa952234a8c688bbbc
Saroar <saroar9@gmail.com>
2015-06-03 17:12:42 +0300

    after replace squares instaaed of b

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -16,44 +16,54 @@
 
 
 def initialize_board
-  b = {}
-  (1..9).each { |position| b[position] = ' ' }
-  b
+  squares = {}
+  (1..9).each { |position| squares[position] = ' ' }
+  squares
 end
 
-def draw_board(b)
+def draw_board(squares)
   system 'clear'
-  puts "  #{b[1]} | #{b[2]}  | #{b[3]}  "
+  puts "  #{squares[1]} | #{squares[2]}  | #{squares[3]}  "
   puts "----+----+-----"
-  puts "  #{b[4]} | #{b[5]}  | #{b[6]}  "
+  puts "  #{squares[4]} | #{squares[5]}  | #{squares[6]}  "
   puts "----+----+-----"
-  puts "  #{b[7]} | #{b[8]}  | #{b[9]}  "
+  puts "  #{squares[7]} | #{squares[8]}  | #{squares[9]}  "
 end
 
-def empty_position(b)
-  b.select { |k,v| v == ' ' }.keys
+def empty_position(squares)
+  squares.select { |k,v| v == ' ' }.keys
 end
 
-def empty?(b, space)
-  empties = empty_position(b)
+def empty?(squares, space)
+  empties = empty_position(squares)
   empties.include?(space)
 end
 
-def player_picks_square(b)
+# def player_picks_square(squares)
+#   puts "Pick a square (1 - 9)"
+#   position = gets.chomp.to_i
+#   until empty?(squares, position)
+#   puts "Place is taken, choose a free space?"
+#   position = gets.chomp.to_i
+#   end
+#   squares[position] = 'x'
+# end
+
+def player_picks_square(squares)
   puts "Pick a square (1 - 9)"
   position = gets.chomp.to_i
-  until empty?(b, position)
+  until empty?(squares, position)
   puts "Place is taken, choose a free space?"
   position = gets.chomp.to_i
   end
-  b[position] = 'x'
+  squares[position] = 'x'
 end
 
 
-def computer_picks_square(b)
+def computer_picks_square(squares)
   sleep 0.5
-  position = empty_position(b).sample
-  b[position] = 'o'
+  position = empty_position(squares).sample
+  squares[position] = 'o'
 end
 
 WINNER_LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
@@ -66,11 +76,11 @@ def two_in_a_row(hsh, mrkr)
   end
 end
 
-def check_winner(b)
+def check_winner(squares)
       WINNER_LINES.each do |line|
 
-      return "Player" if b[line[0]] == 'x' and b[line[1]] == 'x' and b[line[2]] == 'x'
-      return "Computer" if b[line[0]] == 'o' and b[line[1]] == 'o' and b[line[2]] == 'o'
+      return "Player" if squares[line[0]] == 'x' and squares[line[1]] == 'x' and squares[line[2]] == 'x'
+      return "Computer" if squares[line[0]] == 'o' and squares[line[1]] == 'o' and squares[line[2]] == 'o'
     end
     nil
   end
